f8e7f910e58f14195afe36505a3815330ec038c4
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-04-24 14:24:07 -0700

    Create variable.tf

---
+++ EKS/variable.tf
@@ -0,0 +1,31 @@
+variable "cluster_name" {
+    description = "Name of Cluster"
+    type = string
+    default = "Hogwarts"
+}
+
+variable "cluster_version" {
+    description = "EKS Cluster Version"
+    type = number
+    default = "1.24"
+  
+}
+
+#variable "cluster_role" {
+ #   description = "Role ID For Cluster"
+  #  type = string
+   # default = "arn:aws:iam::083772204804:role/EKS"
+#}
+
+variable "vpc_security_group_id" {
+    description = "EKS-4 security group id"
+    type = list(string)
+    default = ["sg-0e4fdbbceb669c049"]
+  
+}
+
+variable "subnet_ids" {
+    description = "EKS VPC ID"
+    type = list(string)
+    default = ["subnet-01c8aa22c96ffe76b", "subnet-0ea37024c6b112d17"]
+}
