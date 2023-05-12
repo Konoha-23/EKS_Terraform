0857c3bc80332dabcafe14c027ce62a903ad030f
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-05-12 12:18:13 -0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,8 @@ module "vpc" {
   azs = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets = ["10.0.4.0/24", "10.0.6.0/24"]
-
+  map_public_ip_on_launch = true
+  
   enable_nat_gateway = true
   single_nat_gateway = true
   one_nat_gateway_per_az = false
@@ -19,6 +20,13 @@ module "vpc" {
   tags = {
     Environment = "test"
     name = "SSJ_VPC"
+  }
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
   }
 
 }
