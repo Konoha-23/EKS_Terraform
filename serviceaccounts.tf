3a07af881393b8c88ed8d62aad0603cb7d389d62
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-05-12 18:20:12 -0700

    Update serviceaccounts.tf

--- serviceaccounts.tf
+++ serviceaccounts.tf
@@ -16,11 +16,11 @@ module "ebs_csi_irsa_role" {
   }
 }
 
-module "load_balancer_controller_targetgroup_binding_only_irsa_role" {
+module "load_balancer_controller_irsa_role" {
   source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
 
-  role_name                                                       = "Goku_load-balancer-controller-targetgroup-binding-only"
-  attach_load_balancer_controller_targetgroup_binding_only_policy = true
+  role_name                              = "load-balancer-controller"
+  attach_load_balancer_controller_policy = true
 
   oidc_providers = {
     ex = {
@@ -33,6 +33,8 @@ module "load_balancer_controller_targetgroup_binding_only_irsa_role" {
     name = "kubernetes.io/cluster/SSJ"
   }
 }
+
+#REF: https://aws-ia.github.io/terraform-aws-eks-blueprints/v4.31.0/add-ons/aws-load-balancer-controller/
 
 module "vpc_cni_ipv4_irsa_role" {
   source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
@@ -70,3 +72,26 @@ module "appmesh_controller_irsa_role" {
     name = "kubernetes.io/cluster/SSJ"
   }
 }
+  
+  
+  
+# DO NOT USE LOAD BALANCER WITH TARGET GROUP BINDING ONLY. IT IS TO ATTACH A LOAD BALANCER TO AN EXISTING ALB AND NLB.
+# Ref: https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.1/guide/targetgroupbinding/targetgroupbinding/
+  
+  #module "load_balancer_controller_targetgroup_binding_only_irsa_role" {
+  #source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  #role_name                                                       = "Goku_load-balancer-controller-targetgroup-binding-only"
+  #attach_load_balancer_controller_targetgroup_binding_only_policy = true
+
+  #oidc_providers = {
+   # ex = {
+    #  provider_arn               = module.eks.oidc_provider_arn
+     # namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+   # }
+ # }
+
+  #tags = {
+   # name = "kubernetes.io/cluster/SSJ"
+  #}
+#}
