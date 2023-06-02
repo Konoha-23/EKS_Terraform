d2cebe8d19aefc0493d10577a457c2038f3a5091
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-06-02 07:41:31 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,26 @@
+#VPC & Security Group Outputs
+
+output "vpc_id" {
+    description = "Practice VPC ID"
+    value = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+    description = "Public Subnet IDS for Practice VPC"
+    value = module.vpc.public_subnets
+}
+
+output "private_subnet_ids" {
+    description = "Private Subnet IDs for Practice VPC"
+    value = module.vpc.private_subnets 
+}
+  
+output "vpc_security_group_id" {
+    description = "Private Subnet IDs for Practice VPC"
+    value = module.security-group.security_group_id
+}
+
+
 ###############################################################################
 # Cluster
 ################################################################################
