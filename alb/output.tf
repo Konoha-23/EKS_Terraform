a295762e25f9ecfe7178f86f90337fd89e353748
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-06-02 08:04:42 -0700

    Delete output.tf

--- alb/output.tf
+++
@@ -1,9 +0,0 @@
-output "Practice_Public_Instance_ID" {
-    description = "ID for Practice_Public Instance"
-    value = aws_instance.Practice_Public.id
-}
-
-output "Practice_Private_Instance_ID" {
-    description = "ID for Practice_Private Instance"
-    value = aws_instance.Practice_Private.id  
-}
