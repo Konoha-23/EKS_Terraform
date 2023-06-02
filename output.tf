1fd17da89a01bd6ca90cb8e2818378b73e94d3e8
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-06-02 08:06:38 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,5 +1,7 @@
 #VPC & Security Group Outputs
 
+#VPC OUTPUTS
+
 output "vpc_id" {
     description = "Practice VPC ID"
     value = module.vpc.vpc_id
@@ -14,9 +16,23 @@ output "private_subnet_ids" {
     description = "Private Subnet IDs for Practice VPC"
     value = module.vpc.private_subnets 
 }
-  
+
+#Security Group Output 
+    
 output "vpc_security_group_id" {
     description = "Private Subnet IDs for Practice VPC"
     value = module.security-group.security_group_id
+}
+    
+#Instance Outputs
+    
+output "Practice_Public_Instance_ID" {
+    description = "ID for Practice_Public Instance"
+    value = aws_instance.Practice_Public.id
+}
+
+output "Practice_Private_Instance_ID" {
+    description = "ID for Practice_Private Instance"
+    value = aws_instance.Practice_Private.id  
 }
 
