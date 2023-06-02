1ee2c82bb5593e73259b449b51f46d5d26cae929
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-06-02 07:40:17 -0700

    Create output.tf

---
+++ alb/output.tf
@@ -0,0 +1,9 @@
+output "Practice_Public_Instance_ID" {
+    description = "ID for Practice_Public Instance"
+    value = aws_instance.Practice_Public.id
+}
+
+output "Practice_Private_Instance_ID" {
+    description = "ID for Practice_Private Instance"
+    value = aws_instance.Practice_Private.id  
+}
