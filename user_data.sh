f6391d4b5ecb732f63a7fda9e0a8b3732418b1a5
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-06-06 09:59:45 -0700

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -11,3 +11,17 @@ sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
 sudo apt-get install terraform
 
+
+---
+#! /bin/bash
+sudo apt update -y
+sudo apt install awscli -y
+sudo adduser ansible
+echo "ansible:ansible" | chpasswd
+echo "ansible ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ansible
+sudo su - ansible
+sudo hostnamectl set-hostname ansible
+sudo apt-add-repository ppa:ansible/ansible #Needed to get /etc/ansible files e.g ansible.cfg
+sudo apt install ansible -y
+sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
+sudo service sshd restart
