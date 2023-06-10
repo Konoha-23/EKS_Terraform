d2cc673b48aabac677988c1847ca4a74f3b17d4d
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-06-10 02:28:16 -0700

    Create output.tf

---
+++ ecr/output.tf
@@ -0,0 +1,14 @@
+output "ecr_url" {
+  description = "URL for ECR private repository"
+  value = module.ecr.repository_url
+}
+  
+output "ecr_arn" {
+  description = "URL for ECR private repository"
+  value = module.ecr.repository_arn
+}
+  
+output "ecr_arn" {
+  description = "URL for ECR private repository"
+  value = module.ecr.repository_registry_id
+}
