b59a8653ea5962b6181a0743c8caba7aaa260182
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-06-16 00:18:33 -0700

    Update add_ons.tf

--- eks/add_ons.tf
+++ eks/add_ons.tf
@@ -1,5 +1,47 @@
 # As of practice on 05/12/23 This did not and may require extra configuration for the namespace and/or service account configuration
 # Study setup more and corresponding lecture from Prof to confirm. 
+#Update 06/16/23 Ref: https://aws.amazon.com/blogs/containers/bootstrapping-clusters-with-eks-blueprints/ #Scroll to using eks blueprints
+
+
+module "eks_blueprints" {
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.0.2"
+
+  # EKS Cluster VPC and Subnet mandatory config
+  vpc_id             = <vpc_id>
+  private_subnet_ids = <private_subnet_ids>
+
+  # EKS CLUSTER VERSION
+  cluster_version = "1.21"
+
+  # EKS MANAGED NODE GROUPS
+  managed_node_groups = {
+    mg_5 = {
+      node_group_name = "managed-ondemand"
+      instance_types  = ["t2.medium"]
+      min_size        = "2"
+    }
+  }
+}
+
+# Add-ons
+module "kubernetes_addons" {
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.0.2"
+
+  eks_cluster_id = module.eks_blueprints.eks_cluster_id
+
+  # EKS Add-ons
+  enable_amazon_eks_vpc_cni            = true
+  enable_amazon_eks_coredns            = true
+  enable_amazon_eks_kube_proxy         = true
+  enable_amazon_eks_aws_ebs_csi_driver = true
+
+  # Self-managed Add-ons
+  enable_aws_for_fluentbit            = true
+  enable_aws_load_balancer_controller = true
+  enable_aws_efs_csi_driver           = true
+  enable_cluster_autoscaler           = true
+  enable_metrics_server               = true
+}
 
 
 
