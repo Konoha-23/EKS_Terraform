e16b27b1885facc24a0c20b5485843da655cb23e
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-06-16 00:57:32 -0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -26,10 +26,12 @@ module "vpc" {
   enable_dns_support = true #Required
 
   public_subnet_tags = {
+    "kubernetes.io/cluster/$(var.cluster_name}" = "shared"
     "kubernetes.io/role/elb" = 1 #Necessary for load balancer
   }
 
   private_subnet_tags = {
+    "kubernetes.io/cluster/$(var.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb" = 1 #Necessary for Load Balancer
   }
 }
