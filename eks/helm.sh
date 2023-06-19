213105ef4252502bab52237aa02266f8e28d27f8
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-06-18 19:27:25 -0700

    Update helm.sh

--- eks/helm.sh
+++ eks/helm.sh
@@ -1,3 +1,4 @@
+@Assumes user has sudo access from being in sudoer group.
 #! /bin/bash
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
 chmod 700 get_helm.sh
