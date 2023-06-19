1f70c222ad445f0ad7435edbfcc353d37618c801
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-06-19 10:33:25 -0700

    Create prometheus_grafana.tf

---
+++ eks/helm/prometheus_grafana.tf
@@ -0,0 +1,25 @@
+monitoring (micro-service) applications with  
+Prometheus and Grafana
+======================================
+deploy prometheus using helm:
+  https://github.com/myLandmakTechnology/prometheus-grafana-ELK-EFK
+kubectl create ns apm
+      #https://charts.helm.sh/stable - old version deprecated but used during class video
+new chart: helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+  
+  kubectl config set-context <context_name> -- namespace=<ns_name>
+  kubectl config set-context current --namespace=apm 
+  kubectl config set-context --current --ns=dev
+  kubectl config set-context --current --ns=apm
+ 
+helm repo add apm https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install prometheus apm/prometheus -f prometheus-value.yml -n apm
+kubectl get pod -n apm
+kubectl get all -n apm
+
+install grafana repo using helm:
+helm repo add visual https://grafana.github.io/helm-charts
+helm repo update
+deploy grafana using helm:
+helm install grafana  visual/grafana -n apm
