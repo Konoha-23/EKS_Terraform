7181617c38f86df9303d110fb7394b0b9701863c
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-06-19 06:46:04 -0700

    Update ngnix.tf

--- eks/helm/ngnix.tf
+++ eks/helm/ngnix.tf
@@ -5,7 +5,26 @@ resource "helm_release" "nginx" {
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "nginx"
 
-  values = [
+  values = [ #This is for customization if you want to pass edits to the default chart
     file("${path.module}/nginx-values.yaml")
   ]
 }
+
+---
+#Instructions for installing nginx ingress controller
+Deploying 3rd party applications using Helm   
+Install Nginx ingress using helm:  
+===============================
+ https://helm.nginx.com/stable
+ helm repo add nginx-ingress https://helm.nginx.com/stable 
+ helm repo update
+ helm search repo nginx-ingress     
+ helm install  nginx    nginx-ingress/nginx-ingress -n nginx
+
+resource "helm_release" "nginx" {
+  name       = "nginx"
+  repository = "https://helm.nginx.com/stable"
+  chart      = "nginx-ingress/nginx-ingress"
+  ]
+}
+
