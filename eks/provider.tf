9735bc907475dafe4954ce6a75c75e50686f4a83
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-06-28 12:36:08 -0700

    Update provider.tf

--- eks/provider.tf
+++ eks/provider.tf
@@ -1,6 +1,7 @@
 ---
 #Latest Version as of 06/19/23
 @Allows you to create an EKS cluster with Helm enabled to install 3rd party packages as add-ons such as Nginx Ingress Controller, Prometheus, etc...
+Terraform Kubernetes Provider reference: https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs
 terraform {
   required_version = "~> 1.0"
   required_providers {
@@ -25,17 +26,15 @@ provider "aws" {
     region = "us-east-2"
 }
 
-provider "kubernetes" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-      command     = "aws"
+provider "helm" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+     api_version = "client.authentication.k8s.io/v1beta1"
+     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+     command     = "aws"
     }
   }
-}
 ---
 terraform {
   required_version = "~> 1.0"
