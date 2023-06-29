f70d41e495c9f4097f1e2718bf8019fff96ddfab
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-06-29 12:03:38 -0700

    Update eks.tf

--- eks/eks.tf
+++ eks/eks.tf
@@ -3,6 +3,67 @@
 #Ref for public and private tagging rules tied to alb controller: https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.4/deploy/subnet_discovery/
 #Addons Version Ref: https://docs.aws.amazon.com/eks/latest/userguide/service-accounts.html#boundserviceaccounttoken-validated-add-on-versions
 
+---
+Update as of 06/29/23
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.13.1"
+
+  cluster_name = var.cluster_name
+  cluster_version = var.cluster_version
+  vpc_id = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+  cluster_endpoint_public_access = var.cluster_endpoint_public_access
+  enable_irsa = var.irsa
+  cluster_addons = {#Relies on default policy in module variable file that lists Worker Node, VPC-CNI, and EC2container policies. If not using the eks managed node group block, these policies need to be entered
+#manually with the iam_role_additional_policies block in which the arn of the needed policies, not the arn of the roles, will be inputted in a key-value format e.g EBS = "EBSDriverPolicy arn"
+    coredns = {
+      most_recent = var.coredns 
+    }
+    kube-proxy = {
+      most_recent = var.kube_proxy
+    }
+    vpc-cni = {
+      most_recent = var.vpc_cni
+    }
+    aws-ebs-csi-driver = {
+      most_recent = var.aws-ebs-csi-driver #Required to install EBS CSI Driver needed for provisioning EBS volumes for persistent volume storage config. This is also tied to the service account created separately
+    }
+  }
+  
+  eks_managed_node_groups = {
+    Goku = {
+      
+      instance_types = var.instance_types
+      ebs_optimized = var.ebs_optimized
+      disk_size = var.disk_size
+      use_custom_launch_template = var.use_custom_launch_template
+      remote_access = {
+                        ec2_ssh_key  = var.key_name
+                      }
+      iam_role_additional_policies = {
+                                        EBS          = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy" #Required for Nodes to have permission to access the EBS CSI Driver addon that was installed.
+                                        LoadBalancer = "arn:aws:iam::083772204804:policy/AmazonEKS_AWS_Load_Balancer_Controller-20230608112628756700000002"
+                                        ExternalDNS  = "arn:aws:iam::083772204804:policy/AmazonEKS_External_DNS_Policy-20230430205147696100000003"
+                                      }
+
+      min_size     = var.min_size
+      max_size     = var.max_size
+      desired_size = var.desired_size
+      update_config = {
+                        max_unavailable = var.max_unavailable
+                      }
+    }
+  }
+  
+  tags = {
+    "kubernetes.io/cluster/SSJ3" = "owned"
+    name = "kubernetes.io/cluster/SSJ3"
+  }
+}
+
+
 --- 
 #06/22/23
 
