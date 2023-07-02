1bf39c943cd726f4486ebbef8aa60f114842740d
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-07-02 16:15:21 -0700

    Update amazon_prometheus.tf

--- eks/helm/prometheus/amazon_prometheus.tf
+++ eks/helm/prometheus/amazon_prometheus.tf
@@ -22,3 +22,13 @@ resource "helm_release" "prometheus" { #aws_managed_prometheus_service
   }
 
 }
+
+#Ref: https://developer.hashicorp.com/terraform/tutorials/kubernetes/helm-provider
+===
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  create_namespace = true
+  namespace  = "visual"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+}
