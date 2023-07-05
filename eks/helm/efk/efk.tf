a3661a67f298cece9c1cda559e4f615c4b1df84f
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-07-05 10:59:56 -0700

    Update and rename kibana.tf to efk.tf

--- eks/helm/efk/efk.tf
+++ eks/helm/efk/efk.tf
@@ -11,7 +11,15 @@ helm install elasticsearch elastic/elasticsearch
    It will deploy elasticsearch in the default namespace 
    It will deploy elasticsearch using default value file 
 helm install elasticsearch elastic/elasticsearch \
-  -n efk -f elasticsearch.values 
+  -n efk -f elasticsearch.values
+
+NOTES:
+1. Watch all containers come up.
+  $ kubectl get pods --namespace=efk -l release=kibana1 -w
+2. Retrieve the elastic user's password.
+  $ kubectl get secrets --namespace=efk elasticsearch-master-credentials -ojsonpath='{.data.password}' | base64 -d
+3. Retrieve the kibana service account token.
+  $ kubectl get secrets --namespace=efk kibana1-kibana-es-token -ojsonpath='{.data.token}' | base64 -d
 
 # ingress2.yml
 apiVersion: extensions/v1beta1  # networking.k8s.io/v1
