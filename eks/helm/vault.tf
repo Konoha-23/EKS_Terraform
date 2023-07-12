ebd6081d405c3258ee8fb837ced71eda54c67e8c
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-07-11 19:49:28 -0700

    Update vault.tf

--- eks/helm/vault.tf
+++ eks/helm/vault.tf
@@ -1,4 +1,5 @@
 Install Vault to EKS via Helm Ref: https://developer.hashicorp.com/vault/tutorials/kubernetes/kubernetes-amazon-eks
+Follow Process in reference to setup Vault Cluster after installation
 
 resource "helm_release" "vault" {
   name       = "vault"
@@ -16,3 +17,7 @@ resource "helm_release" "vault" {
     value = "true"
   }
 }
+
+
+To get the root token needed to log in from the website.
+cat cluster-keys.json | jq -r ".root_token"
