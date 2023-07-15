3c195aa377398fd19317bda6476b2a9f629dd5a5
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-07-14 23:18:54 -0700

    Create sg.tf

---
+++ eks/helm/efs/sg.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "allow_nfs" {
+  name        = "allow nfs for efs"
+  description = "Allow NFS inbound traffic"
+  vpc_id      = local.vpc_id
+
+  ingress {
+    description = "NFS from VPC"
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = [local.vpc_cidr]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+}
