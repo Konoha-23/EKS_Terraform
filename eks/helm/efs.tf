36c11319c5876a511aab6d0107b59e20a4f30a08
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-07-14 22:42:08 -0700

    Update efs.tf

--- eks/helm/efs.tf
+++ eks/helm/efs.tf
@@ -1,3 +1,5 @@
+
+
 Ref: https://github.com/kubernetes-sigs/aws-efs-csi-driver/blob/master/docs/iam-policy-create.md
 
 resource "helm_release" "efs" {
@@ -18,3 +20,14 @@ resource "helm_release" "efs" {
 }
 
 EFS Driver Policy: arn:aws:iam::aws:policy/aws-service-role/AmazonElasticFileSystemServiceRolePolicy
+
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  labels:
+    app.kubernetes.io/name: aws-efs-csi-driver
+  name: efs-csi-controller-sa
+  namespace: kube-system
+  annotations:
+    eks.amazonaws.com/role-arn: arn:aws:iam::111122223333:role/EKS_EFS_CSI_DriverRole
