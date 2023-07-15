06b4bf4daa23ef1e66006cb400626bfe682541db
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-07-14 22:34:39 -0700

    Create efs.tf

---
+++ eks/helm/efs.tf
@@ -0,0 +1,18 @@
+Ref: https://github.com/kubernetes-sigs/aws-efs-csi-driver/blob/master/docs/iam-policy-create.md
+
+resource "helm_release" "efs" {
+  name       = "efs"
+  namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/aws-efs-csi-driver/"
+  chart      = "aws-efs-csi-driver"
+
+    set {
+    name  = "controller.serviceAccount.create"
+    value = "false"
+  } 
+
+  set {
+    name  = "controller.serviceAccount.name"
+    value = "efs-csi-controller-sa"
+  }
+}
