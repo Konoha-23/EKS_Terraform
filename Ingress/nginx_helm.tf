1e9923419ba3915c2765050ab52000590f98bab0
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-10-15 10:09:37 -0700

    Update nginx_helm.tf

--- Ingress/nginx_helm.tf
+++ Ingress/nginx_helm.tf
@@ -1,5 +1,16 @@
 resource "helm_release" "nginx" {
   name       = "nginx-ingress"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+}
+
+#Update as of 10/15/23
+#https://artifacthub.io/packages/helm/ingress-nginx/ingress-nginx
+#https://github.com/kubernetes/ingress-nginx/tree/main/charts/ingress-nginx
+
+---
+resource "helm_release" "nginx" {
+  name       = "nginx-ingress"
   create_namespace = true
   namespace  = "nginx"
   repository = "https://helm.nginx.com/stable"
