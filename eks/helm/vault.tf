88f028f0f21e3d4a23f9a25cf4273cd2f24eca03
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-01-12 09:48:55 -0800

    Update vault.tf

--- eks/helm/vault.tf
+++ eks/helm/vault.tf
@@ -1,5 +1,7 @@
 Install Vault to EKS via Helm Ref: https://developer.hashicorp.com/vault/tutorials/kubernetes/kubernetes-amazon-eks
 Inject Kubernetes Secrets: https://developer.hashicorp.com/vault/tutorials/kubernetes/kubernetes-sidecar
+Agent-Injector-Sidecar Annotations Ref: https://developer.hashicorp.com/vault/docs/platform/k8s/injector#annotations 
+Update as of 01/2024
 Follow Process in reference to setup Vault Cluster after installation
 
 resource "helm_release" "vault" {
