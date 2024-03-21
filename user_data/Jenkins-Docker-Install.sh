f354223e49ce15e1fa9434904702c55318e5955a
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-03-21 12:53:32 -0700

    Updated Setup for installing Jenkins and Docker on an Ubuntu server during startup

--- user_data/Jenkins-Docker-Install.sh
+++ user_data/Jenkins-Docker-Install.sh
@@ -1,20 +1,25 @@
 #Jenkins and Docker Installation with java open-jdk in ec2 resource configuration "user_data = file("${path.module}/user_data/Jenkins-Docker-Install.sh")"          
 #!/bin/bash
 sudo hostname docker
-sudo apt update -y 
-sudo apt install docker.io -y
-sudo usermod -aG docker ubuntu 
+sudo adduser jenkins
+sudo adduser docker
+sudo su - jenkins
+sudo apt update -y  
 sudo apt install wget -y
 sudo apt update -y
-sudo apt install openjdk-11-jdk -y
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
+sudo apt install openjdk-17-jdk -y
+sudo apt update -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y
 sudo apt install jenkins -y
 sudo systemctl start jenkins
 sudo echo "jenkins ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/jenkins
+sudo apt install docker.io -y
 sudo usermod -aG docker jenkins
-sudo su - jenkins
 sudo chown jenkins /var/run/docker.sock
 sudo systemctl restart docker.service
 
